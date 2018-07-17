092d871eac9bce682b092b1196fceba0e81442cd
smkim89 <kiak357@gmail.com>
2018-07-17 23:12:02 +0900

    styled-component 추가.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import styled, {injectGlobal} from 'styled-components';
+
+injectGlobal`
+  body{
+    margin : 0;
+    padding : 0;
+  }
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <Fragment>
+        <Container>
+          <Button danger> danger </Button>
+          <Button success> success </Button>
+          <Anchor href="http://www.naver.com"> NAVER </Anchor>
+        </Container>
+      </Fragment>
     );
   }
 }
+
+//styled.div -> html 요소를 선택함.
+const Container = styled.div`
+  height : 100vh;
+  width : 100%;
+  background-color : red;
+`;
+
+const Button = styled.button`
+  border-radius : 50px;
+  padding : 5px;
+  min-width : 120px;
+  color : red;
+  font-weight : 600px;
+  webkit-appearance : none;
+  cursor : pointer;
+  &:active,
+  &:focus {
+    outline : none;
+  }
+  background-color : ${props => props.danger ? "black" : "white"}
+`;
+
+//Button 컴포넌트에 a태그 기능 추가
+const Anchor = Button.withComponent("a").extend`
+  text-decoration : none;
+`
+
 
 export default App;
